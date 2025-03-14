898d3ff2f333ddea8d0cfc3baad7444050b366f2
Eyuzikfr <eyuzikchhunzu2@gmail.com>
2025-03-14 19:24:47 +0545

    add languages container and install nanoid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import Header from "./components/Header";
+import { languages } from "./components/languages";
+import { nanoid } from "nanoid";
 import "./css/App.css";
 
 export default function App() {
+  const languageList = languages.map((lang) => (
+    <div
+      key={nanoid()}
+      className={lang.name + " language"}
+      style={{ backgroundColor: lang.backgroundColor, color: lang.color }}
+    >
+      {lang.name}
+    </div>
+  ));
+
   return (
     <main>
-      <Header />
+      <div className="header-and-status">
+        <Header />
+        <div className="status-message">
+          <h2 className="main-status">You win!</h2>
+          <p className="sub-status">Well done! 🎉</p>
+        </div>
+      </div>
+      <div className="languages-container">{languageList}</div>
     </main>
   );
 }
