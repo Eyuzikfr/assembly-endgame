4f8f0edd543aab47ace372af1a939a376a9efc84
Eyuzikfr <eyuzikchhunzu2@gmail.com>
2025-03-15 11:51:47 +0545

    add remaining page elements and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import Header from "./components/Header";
 import { languages } from "./components/languages";
+import { useState } from "react";
 import { nanoid } from "nanoid";
 import "./css/App.css";
 
 export default function App() {
-  const languageList = languages.map((lang) => (
-    <div
+  const [currentWord, setCurrentWord] = useState("react");
+  const alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+  const languageElements = languages.map((lang) => (
+    <span
       key={nanoid()}
       className={lang.name + " language"}
       style={{ backgroundColor: lang.backgroundColor, color: lang.color }}
     >
       {lang.name}
+    </span>
+  ));
+
+  const letterElements = currentWord.split("").map((letter, index) => (
+    <div key={index} className="letter">
+      {letter.toUpperCase()}
     </div>
   ));
+
+  const keyboardElements = alphabets
+    .split("")
+    .map((letter, index) => (
+      <button key={index}>{letter.toUpperCase()}</button>
+    ));
 
   return (
     <main>
@@ -23,7 +39,10 @@ export default function App() {
           <p className="sub-status">Well done! 🎉</p>
         </div>
       </div>
-      <div className="languages-container">{languageList}</div>
+      <div className="languages-container">{languageElements}</div>
+      <div className="word-display">{letterElements}</div>
+      <div className="keyboard">{keyboardElements}</div>
+      <button className="new-game-btn">New Game</button>
     </main>
   );
 }
